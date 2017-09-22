b163c670d75bedad8e6eb54035ca7e836a84e90f
Jesus Izquierdo <izquierdo843@gmail.com>
2017-09-22 18:48:37 -0400

    visuals improved for login part 2

--- app/src/main/java/com/example/jesusizquierdo/debatethis/RecycleViewAdapters/FirebaseDebateCardViewHolder.java
+++ app/src/main/java/com/example/jesusizquierdo/debatethis/RecycleViewAdapters/FirebaseDebateCardViewHolder.java
@@ -37,10 +37,10 @@ public class FirebaseDebateCardViewHolder extends RecyclerView.ViewHolder  {
         TextView userName = (TextView) itemView.findViewById(R.id.tv_name_of_card_creator_discussionCard);
 
         articleTitle.setText(debateCard.getArticleTitle());
-        category.setText(debateCard.getCategory());
-        cardTitle.setText(debateCard.getTitle());
-        userName.setText("Discussion started by "+ debateCard.getUserName());
-        source.setText("Based of article from/by: " +debateCard.getSource());
+        //category.setText(debateCard.getCategory());
+        //cardTitle.setText(debateCard.getTitle());
+       // userName.setText("Discussion started by "+ debateCard.getUserName());
+       // source.setText("Based of article from/by: " +debateCard.getSource());
 
         if(TextUtils.isEmpty(debateCard.getUserDescription())){
             description.setHeight(0);
